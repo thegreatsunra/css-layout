9f99ff2946a62364c6c866290a58bf253fe156da
Dane Petersen <thegreatsunra@gmail.com>
2018-02-11 16:34:07 -0600

    Add entry and outputf for JS and HTML

--- webpack.config.js
+++ webpack.config.js
@@ -1,2 +1,16 @@
+const path = require('path')
+
 module.exports = {
+  entry: [
+    './src/index.js',
+    './src/index.html'
+  ],
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+    ]
+  }
 }
