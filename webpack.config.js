7e23b5d556b93f798d874fb3516df986586b2f39
Dane Petersen <thegreatsunra@gmail.com>
2018-02-13 09:03:32 -0600

    Add loaders for images and media

--- webpack.config.js
+++ webpack.config.js
@@ -51,6 +51,22 @@ module.exports = {
         })
       },
       {
+        test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+        loader: 'url-loader',
+        options: {
+          limit: 10000,
+          name: 'images/[name].[hash:7].[ext]'
+        }
+      },
+      {
+        test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/,
+        loader: 'url-loader',
+        options: {
+          limit: 10000,
+          name: 'media/[name].[hash:7].[ext]'
+        }
+      },
+      {
         test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
         loader: 'url-loader',
         options: {
