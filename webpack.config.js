7a285b18b5fa6a8e5d17c1a915aa3fbfa6dae072
Dane Petersen <thegreatsunra@gmail.com>
2018-02-13 09:03:03 -0600

    Add loader and rule for handling font files

--- webpack.config.js
+++ webpack.config.js
@@ -49,6 +49,14 @@ module.exports = {
             loader: 'postcss-loader'
           }]
         })
+      },
+      {
+        test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+        loader: 'url-loader',
+        options: {
+          limit: 10000,
+          name: 'fonts/[name].[hash:7].[ext]'
+        }
       }
     ]
   },
