9e37229b8e9cdb0e4b4a172e1cb7abd789c4d78e
Dane Petersen <thegreatsunra@gmail.com>
2018-02-15 20:44:14 -0600

    Add vue js boilerplate, app component, target ID, default route info

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <!-- <img class="zomgimage" src="assets/images/logo.png"> -->
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style scoped>
+  .zomgimage {
+    border: 1px solid #f00;
+  }
+</style>
--- src/index.js
+++ src/index.js
@@ -1,2 +1,16 @@
+import Vue from 'vue'
+
+import App from './App.vue'
 import css from './styles/index.css'
-import js from './scripts/index.js'
+// import js from './scripts/index.js'
+import router from './router'
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  template: '<App/>',
+  components: { App }
+})
