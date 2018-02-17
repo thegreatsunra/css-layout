2a7eb93961cc5c98e59773d84921f28bc50465ba
Dane Petersen <thegreatsunra@gmail.com>
2018-02-16 20:17:49 -0600

    Add @ alias for linking javascript assets as though from src folder

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,8 @@ module.exports = {
   resolve: {
     extensions: ['.js', '.vue', '.json'],
     alias: {
-      'vue$': 'vue/dist/vue.esm.js'
+      'vue$': 'vue/dist/vue.esm.js',
+      '@': path.join(__dirname, 'src')
     }
   },
   module: {
