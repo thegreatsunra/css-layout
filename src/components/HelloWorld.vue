1ace77a94b9484d07ae44eacda6999f7e4b964d8
Dane Petersen <thegreatsunra@gmail.com>
2018-02-18 20:01:33 -0600

    Add button styles and examples for inputs, hyperlinks and buttons

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,6 +2,33 @@
   <div class="view">
     <h1>Hello World</h1>
     <p>{{ msg }}</p>
+    <p>
+      <input type="submit" value="Button" class="btn">
+      <input type="submit" value="Button" class="btn btn--primary">
+      <input type="submit" value="Button" class="btn btn--success">
+      <input type="submit" value="Button" class="btn btn--warning">
+      <input type="submit" value="Button" class="btn btn--danger">
+      <input type="submit" value="Button" class="btn btn--inverse">
+    </p>
+
+    <p>
+      <button class="btn">Button</button>
+      <button class="btn btn--primary">Button</button>
+      <button class="btn btn--success">Button</button>
+      <button class="btn btn--warning">Button</button>
+      <button class="btn btn--danger">Button</button>
+      <button class="btn btn--inverse">Button</button>
+    </p>
+
+    <p>
+      <a href="#" class="btn">Button</a>
+      <a href="#" class="btn btn--primary">Button</a>
+      <a href="#" class="btn btn--success">Button</a>
+      <a href="#" class="btn btn--warning">Button</a>
+      <a href="#" class="btn btn--danger">Button</a>
+      <a href="#" class="btn btn--inverse">Button</a>
+    </p>
+
   </div>
 </template>
 
