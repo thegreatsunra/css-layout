7c6d312f3adcb33e7625b653f3d35f3f37451d02
Dane Petersen <thegreatsunra@gmail.com>
2018-02-18 20:03:18 -0600

    Stub out HTML for testing generic styles

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -138,6 +138,20 @@
       <a href="#" class="btn btn--xl btn--inverse">Button</a>
     </p>
 
+
+    <p>This text is normal.</p>
+    <p><strong>This text is strong.</strong></p>
+    <p><em>This text is em.</em></p>
+    <p><a href="#">This text is a link.</a></p>
+    <p><code>This text is code.</code></p>
+
+    <h1>Heading 1</h1>
+    <h2>Heading 2</h2>
+    <h3>Heading 3</h3>
+    <h4>Heading 4</h4>
+    <h5>Heading 5</h5>
+    <h6>Heading 6</h6>
+
   </div>
 </template>
 
